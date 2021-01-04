8d7a19cbedbd3087ffe9aee216abfe6bb1a4d904
Cactus43 <alessandroj.1998@gmail.com>
2021-01-04 20:18:25 +0100

    Create json_test.php

---
+++ json_test.php
@@ -0,0 +1,21 @@
+<?php
+		include_once("open.php");
+    
+//example query
+	$queryU="SELECT id, nickname, nome, cognome, img FROM `user`";
+
+$resultU= mysqli_query($db, $queryU);
+
+$end = array();
+
+while($sql = mysqli_fetch_array($resultU, MYSQLI_ASSOC)){
+
+//adding a null values to test how the table print them
+	$sql["check"]=null;
+	array_push($end, $sql);
+}
+
+	mysqli_close($db);
+	
+	echo json_encode($end);
+?>
